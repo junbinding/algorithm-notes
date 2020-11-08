01589e6898893d39c3ebd82f7b24e3dc6b9d568f
dingjunbin <junbinding@gmail.com>
2020-11-08 20:44:29 +0800

    feat: serialize-and-deserialize-binary-tree

--- recs.serialize-and-deserialize-binary-tree.py
+++ recs.serialize-and-deserialize-binary-tree.py
@@ -1,3 +1,4 @@
+from collections import deque
 
 class TreeNode(object):
     def __init__(self, x):
@@ -12,40 +13,54 @@ class Codec:
     序列化是将一个数据结构或者对象转换为连续的比特位的操作，进而可以将转换后的数据存储在一个文件或者内存中，同时也可以通过网络传输到另一个计算机环境，采取相反方式重构得到原数据。
     请设计一个算法来实现二叉树的序列化与反序列化。这里不限定你的序列 / 反序列化算法执行逻辑，你只需要保证一个二叉树可以被序列化为一个字符串并且将这个字符串反序列化为原始的树结构。
     """
+    # 广度优先
     def serialize(self, root):
-        """Encodes a tree to a single string.
+        queue = [root]
+        res = []
 
-        :type root: TreeNode
-        :rtype: str
-        """
-        if not root:
-            return 'null,'
+        while queue:
+            node = queue.pop(0)
+            if node:
+                print(node.val)
+                res.append(str(node.val))
+                queue.append(node.left)
+                queue.append(node.right)
+            else:
+                print('null')
+                res.append('null')
 
-        left = self.serialize(root.left)
-        right = self.serialize(root.right)
-        return str(root.val) + ',' + left + right
-
+        return ','.join(res)
 
     def deserialize(self, data):
-        """Decodes your encoded data to tree.
+        if data == 'null':
+            return None
 
-        :type data: str
-        :rtype: TreeNode
-        """
-        l = data.split(',')
-        return self.build_tree(l)
+        l = data.split(",")
+        root = TreeNode(l[0])
+        queue = [root]
+        cursor = 1
 
-    def build_tree(self, data):
-        if not data:
-            return
-        val = data.pop(0)
-        if val == 'null': return None
-        node = TreeNode(val)
-        node.left = self.build_tree(data)
-        node.right = self.build_tree(data)
-        return node
+        while cursor < len(l):
+            node = queue.pop(0)
+            left_val = l[cursor]
+            right_val = l[cursor + 1]
+
+            if left_val != 'null':
+                left_node = TreeNode(left_val)
+                node.left = left_node
+                queue.append(left_node)
+
+            if right_val != 'null':
+                right_node = TreeNode(right_val)
+                node.right = right_node
+                queue.append(right_node)
+
+            cursor += 2
+
+        return root
+
 
 so = Codec()
+print(so.serialize(so.deserialize('1,2,3,null,null,4,5,null,null,null,null')))
 
-print(so.serialize(so.deserialize('1,2,3,null,null,4,5')))
-
+print([1, 2, 3,].pop(0))
