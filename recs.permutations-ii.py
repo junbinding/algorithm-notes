1fe073820e3a12e9d37015d7419098e2fe17e5b9
dingjunbin <junbinding@gmail.com>
2020-11-10 08:17:26 +0800

    feat: permutations-ii

--- recs.permutations-ii.py
+++ recs.permutations-ii.py
@@ -3,18 +3,20 @@ from typing import List
 
 class Solution:
     """
-    46. 全排列
-    https://leetcode-cn.com/problems/permutations/
-    给定一个 没有重复 数字的序列，返回其所有可能的全排列。
+    47. 全排列 II
+    https://leetcode-cn.com/problems/permutations-ii/
+    给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。
     """
     # 回溯法
-    def permute(self, nums: List[int]) -> List[List[int]]:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
         res = []
         if not nums:
             return res
 
         l = len(nums)
         used = [False] * l
+
         def backtrack(depth, path):
             if depth == l:
                 res.append(path[:])
@@ -23,18 +25,21 @@ class Solution:
             for i in range(l):
                 if used[i]:
                     continue
+
+                if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
+                    continue
                 # 置换
                 used[i] = True
                 path.append(nums[i])
-                backtrack(depth + 1, path)
-                used[i] = False
+                backtrack(depth+1, path)
                 # 恢复
+                used[i] = False
                 path.pop()
 
         backtrack(0, [])
         return res
 
 
-
 so = Solution()
-print(so.permute([1, 2, 3]))
+print(so.permuteUnique([1,0,0,9]))
+
