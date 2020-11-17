f9b14704a8cdde90fadf7f188525d6f306258ec7
dingjunbin <junbinding@gmail.com>
2020-11-17 08:13:53 +0800

    optimizer: generate-parentheses

--- recs.generate-parentheses.py
+++ recs.generate-parentheses.py
@@ -7,25 +7,28 @@ class Solution:
     https://leetcode-cn.com/problems/generate-parentheses/
     数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
     """
-    arr = []
+    # dfs
     def generateParenthesis(self, n: int) -> List[str]:
-        self.arr = []
-        self._gen(0, 0, n * 2, '')
-        return self.arr
+        res = []
+        cur = ''
 
+        def dfs(left, right, cur):
+            if left == 0 and right == 0:
+                res.append(cur)
+                return
 
-    def _gen(self, left, right, max, s):
-        # 终止条件
-        if len(s) == max:
-            self.arr.append(s)
-            return s
+            if right < left:
+                return
 
-        # 递归
-        if left < max / 2:
-            self._gen(left + 1, right, max, s + '(')
-        if left > right:
-            self._gen(left, right + 1, max, s + ')')
+            if left > 0:
+                dfs(left - 1, right, cur + '(')
+
+            if right > 0:
+                dfs(left, right - 1, cur + ')')
+
+        dfs(n, n, cur)
+        return res
 
 
 so = Solution()
-print(so.generateParenthesis(1))
+print(so.generateParenthesis(3))
