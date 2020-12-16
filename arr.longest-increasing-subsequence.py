335f5bafc1bb4e6cacf6dad3f2e6206f9723c5e3
dingjunbin <junbinding@gmail.com>
2020-12-17 07:18:51 +0800

    feat: longest-increasing-subsequence

--- arr.longest-increasing-subsequence.py
+++ arr.longest-increasing-subsequence.py
@@ -9,6 +9,28 @@ class Solution:
     子序列是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
     """
     def lengthOfLIS(self, nums: List[int]) -> int:
+        # 二分法，res 为最长子序列，tails 代表每个子序列尾部的值
+        size = len(nums)
+        if size < 2:
+            return size
+
+        cell = [nums[0]]
+        for num in nums[1:]:
+            if num > cell[-1]:
+                cell.append(num)
+                continue
+
+            l, r = 0, len(cell) - 1
+            while l < r:
+                mid = l + (r - l) // 2
+                if cell[mid] < num:
+                    l = mid + 1
+                else:
+                    r = mid
+            cell[l] = num
+        return len(cell)
+
+    def lengthOfLISByForce(self, nums: List[int]) -> int:
         if not nums:
             return 0
 
