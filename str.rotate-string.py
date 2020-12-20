ce51382c8e53d1a3f6111f03edcb2d5992d72f66
dingjunbin <junbinding@gmail.com>
2020-12-21 07:27:16 +0800

    feat: rotate-string

---
+++ str.rotate-string.py
@@ -0,0 +1,15 @@
+class Solution:
+    """
+    796. 旋转字符串
+    https://leetcode-cn.com/problems/rotate-string/
+    给定两个字符串, A 和 B。
+    A 的旋转操作就是将 A 最左边的字符移动到最右边。 例如, 若 A = 'abcde'，在移动一次之后结果就是'bcdea' 。如果在若干次旋转操作之后，A 能变成B，那么返回True。
+    """
+    def rotateString(self, A: str, B: str) -> bool:
+        if len(A) != len(B):
+            return False
+        return (B * 2).find(A) != -1
+
+
+so = Solution()
+print(so.rotateString('abcde', 'abced'))
